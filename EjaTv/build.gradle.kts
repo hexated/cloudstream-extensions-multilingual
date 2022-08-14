cf61c718f8516bc4a5b9ea1f3e01cb341acc9b30
Blatzar <46196380+Blatzar@users.noreply.github.com>
2022-08-14 19:40:52 +0200

    Add TvTypes to EjaTv

--- EjaTv/build.gradle.kts
+++ EjaTv/build.gradle.kts
@@ -9,13 +9,12 @@ cloudstream {
     // authors = listOf("Cloudburst")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
     status = 1 // will be 3 if unspecified
-
-    
-    }
+    tvTypes = listOf("Live")
+}
