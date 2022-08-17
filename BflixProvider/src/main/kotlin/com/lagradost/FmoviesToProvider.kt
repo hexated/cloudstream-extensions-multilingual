440d8ef576bf5748335a70e718c7fdbc0c6ba76e
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-08-17 14:32:36 +0200

    remove all english providers

--- BflixProvider/src/main/kotlin/com/lagradost/FmoviesToProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.lagradost
-
-class FmoviesToProvider : BflixProvider() {
-    override var mainUrl = "https://fmovies.to"
-    override var name = "Fmovies.to"
-}
--- BflixProvider/src/main/kotlin/com/lagradost/SflixProProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.lagradost
-
-class SflixProProvider : BflixProvider() {
-    override var mainUrl = "https://sflix.pro"
-    override var name = "Sflix.pro"
-}
