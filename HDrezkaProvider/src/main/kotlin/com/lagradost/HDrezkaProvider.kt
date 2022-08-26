a09f3b23f3d1c7b119fb39972e89087bdbc4eb39
hexated <hexated@gmail.com>
2022-08-26 12:03:16 +0700

    changes HDrezka domain

--- HDrezkaProvider/src/main/kotlin/com/lagradost/HDrezkaProvider.kt
+++ HDrezkaProvider/src/main/kotlin/com/lagradost/HDrezkaProvider.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
-import com.lagradost.cloudstream3.mvvm.safeApiCall
 import com.lagradost.cloudstream3.mvvm.suspendSafeApiCall
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
@@ -16,7 +15,7 @@ import org.jsoup.nodes.Element
 import java.util.*
 
 class HDrezkaProvider : MainAPI() {
-    override var mainUrl = "https://hdrezka19139.org"
+    override var mainUrl = "https://rezka.ag"
     override var name = "HDrezka"
     override val hasMainPage = true
     override var lang = "ru"
