e32a531ed069f5ba2d58739ccd77a554e480dd9e
hexated <hexated@gmail.com>
2022-08-30 05:06:25 +0700

    changes Yomovies domain

--- YomoviesProvider/build.gradle.kts
+++ YomoviesProvider/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 2
+version = 3
 
 
 cloudstream {
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-    // authors = listOf("Cloudburst")
+     authors = listOf("Hexated")
 
     /**
      * Status int as the following:
