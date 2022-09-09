d7e4d030386aa123f8a0002cec2043173f619db0
Hexated <37908684+hexated@users.noreply.github.com>
2022-09-10 00:51:48 +0700

    fixed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,31 @@ val disabled = listOf<String>(
     "EgyBestProvider",
     "FaselHDProvider",
     "AkwamProvider",
-    "MyCimaProvider, AnimeIndoProvider, AnimeSailProvider, Anizm, DramaidProvider, DubokuProvider, Gomunimeis, GomunimeProvider, Hdfilmcehennemi, HDrezkaProvider, IdlixProvider, KuramanimeProvider, KuronimeProvider, LayarKacaProvider, MultiplexProvider, NeonimeProvider, NontonAnimeIDProvider, OploverzProvider, OtakudesuProvider, PhimmoichillProvider, RebahinProvider, TocanimeProvider, UakinoProvider, UseeTv, YomoviesProvider"
+    "MyCimaProvider", 
+    "AnimeIndoProvider", 
+    "AnimeSailProvider", 
+    "Anizm", 
+    "DramaidProvider", 
+    "DubokuProvider", 
+    "Gomunimeis", 
+    "GomunimeProvider", 
+    "Hdfilmcehennemi", 
+    "HDrezkaProvider", 
+    "IdlixProvider", 
+    "KuramanimeProvider", 
+    "KuronimeProvider", 
+    "LayarKacaProvider", 
+    "MultiplexProvider", 
+    "NeonimeProvider", 
+    "NontonAnimeIDProvider", 
+    "OploverzProvider", 
+    "OtakudesuProvider", 
+    "PhimmoichillProvider", 
+    "RebahinProvider", 
+    "TocanimeProvider", 
+    "UakinoProvider", 
+    "UseeTv", 
+    "YomoviesProvider"
 )
 
 File(rootDir, ".").eachDir { dir ->
