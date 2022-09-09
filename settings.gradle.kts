8833a4b686129aaacc6c04d7fc94d74863e5cbaf
Hexated <37908684+hexated@users.noreply.github.com>
2022-09-10 00:21:00 +0700

    moved some providers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,10 @@ rootProject.name = "CloudstreamPlugins"
 
 // Plugins are included like this
 val disabled = listOf<String>(
-    "EgyBestProvider", "FaselHDProvider", "AkwamProvider", "MyCimaProvider"
+    "EgyBestProvider",
+    "FaselHDProvider",
+    "AkwamProvider",
+    "MyCimaProvider, AnimeIndoProvider, AnimeSailProvider, Anizm, DramaidProvider, DubokuProvider, Gomunimeis, GomunimeProvider, Hdfilmcehennemi, HDrezkaProvider, IdlixProvider, KuramanimeProvider, KuronimeProvider, LayarKacaProvider, MultiplexProvider, NeonimeProvider, NontonAnimeIDProvider, OploverzProvider, OtakudesuProvider, PhimmoichillProvider, RebahinProvider, TocanimeProvider, UakinoProvider, UseeTv, YomoviesProvider"
 )
 
 File(rootDir, ".").eachDir { dir ->
